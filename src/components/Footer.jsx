9a2b7e0bdcebb87205989c9a1ff27793334a0644
Royalboe <auomidejohnson@gmail.com>
2022-07-26 12:26:28 +0100

    Added home and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <div>Powered by PwC/Xerde Student &#169; 2022</div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
