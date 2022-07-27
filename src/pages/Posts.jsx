6dde6012343d0f11715b0f34b0d3d5cc33be5741
Royalboe <auomidejohnson@gmail.com>
2022-07-27 11:17:33 +0100

    Added a try catch block

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -9,29 +9,35 @@ const Posts = () => {
   //       .then((posts) => setPosts(posts));
   //   }, []);
 
-  const fetchPosts = async () => {
-    let posts = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await posts.json();
-    setPosts(data);
-  };
-  useEffect(() => {
-    fetchPosts();
-  }, [posts]); // useEffect second parameter is used to track changes in state and rerun
+    const fetchPosts = async () => {
+      try {
+        const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await posts.json();
+        setPosts(data);
+      } catch (err) {
+        console.log(
+          `Sorry, it is not your fault, it is ours\n We got error ${err}.`
+        );
+      }
+    };
+    useEffect(() => {
+      fetchPosts();
+    }, [posts]); // useEffect second parameter is used to track changes in state and rerun
 
-  return (
-    <div>
-      <h1>These are my posts</h1>
-      {posts &&
-        posts.map((post, index) => {
-          return (
-            <div>
-              <h3 key={post.id}>{`Title: ${post.title}`}</h3>
-              <p>{`Body: ${post.body}`}</p>
-            </div>
-          );
-        })}
-    </div>
-  );
+    return (
+      <div>
+        <h1>These are my posts</h1>
+        {posts &&
+          posts.map((post, index) => {
+            return (
+              <div>
+                <h3 key={post.id}>{`Title: ${post.title}`}</h3>
+                <p>{`Body: ${post.body}`}</p>
+              </div>
+            );
+          })}
+      </div>
+    );
 };
 
 export default Posts;
