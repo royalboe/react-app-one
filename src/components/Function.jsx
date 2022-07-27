3ed1a4e2425a50ec6871debc019ba76b4fb00a5e
Royalboe <auomidejohnson@gmail.com>
2022-07-27 09:44:36 +0100

    added file

---
+++ src/components/Function.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h2>Name: Ayomide</h2>
+    </div>
+  );
+};
+
+export function Description() {
+  return (
+    <div>
+      <p>I am a software engineer</p>
+    </div>
+  );
+}
+
+export default Title;
